25088aff28342ccf543b438f6427eb5e80478b64
wssong85 <tabliser@gmail.com>
2018-01-20 17:59:49 +0900

    s

--- init/src/main/java/realtime/shopping/place/service/impl/PlaceServiceImpl.java
+++ init/src/main/java/realtime/shopping/place/service/impl/PlaceServiceImpl.java
@@ -2,9 +2,12 @@ package realtime.shopping.place.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import realtime.shopping.place.mapper.PlaceMapper;
 import realtime.shopping.place.service.PlaceService;
 
+@Service("PlaceService")
 public class PlaceServiceImpl implements PlaceService{
 	
 	@Resource(name = "PlaceMapper")
