eae2c24c4cf8a3030632a28a9bd78ae913ce3b9d
wssong85 <tabliser@gmail.com>
2018-04-07 16:28:40 +0900

    www

--- init/src/main/java/realtime/common/aop/ApiAop.java
+++ init/src/main/java/realtime/common/aop/ApiAop.java
@@ -1,12 +1,48 @@
 package realtime.common.aop;
 
-import org.apache.poi.util.SystemOutLogger;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.aspectj.lang.JoinPoint;
+
+import realtime.shopping.user.mapper.UserMapper;
 
 public class ApiAop {
 	
-	public void loginCheck(JoinPoint joinPoint) throws Exception {
-		System.out.println("1");
+	@Resource(name = "UserMapper")
+	private UserMapper userMapper;
+	
+	public void loginCheck(JoinPoint joinPoint) throws Throwable {
+		
+		Object params[] = joinPoint.getArgs();
+        for(Object param : params) {
+        	
+        	//Map일 경우
+        	if(param instanceof Map) {
+        		
+        		if(userMapper.selectTbUserCountForCheck2((Map<String, Object>)(Object)param) == 0) {
+        			throw new Exception("not user...");
+        		}
+        		
+        	}
+        	
+        	//리스트 일 경우
+        	if(param instanceof List) {
+        		
+        	}
+        }
+
+		
+		
+//		Object retVal = joinPoint.proceed();
+//		if(retVal instanceof List) {
+//		}
+//		if(retVal instanceof Map) {
+//		}
+//		return null;
+		
 	}
 	
 }
--- init/src/main/java/realtime/shopping/user/mapper/UserMapper.java
+++ init/src/main/java/realtime/shopping/user/mapper/UserMapper.java
@@ -7,4 +7,5 @@ import egovframework.rte.psl.dataaccess.mapper.Mapper;
 @Mapper("UserMapper")
 public interface UserMapper {
 	int selectTbUserCountForCheck(Map<String, Object> map);
+	int selectTbUserCountForCheck2(Map<String, Object> map);
 }
